cffabf368e8d1088245d476fd84eaa0b7b38f063
Chris <webremote@users.noreply.github.com>
2016-03-07 17:03:39 +0100

    Update Htmlcache_HtmlcacheService.php

--- services/Htmlcache_HtmlcacheService.php
+++ services/Htmlcache_HtmlcacheService.php
@@ -9,7 +9,7 @@
  * @link      https://github.com/craftapi
  * @package   HTMLCache
  * @since     1.0.1
- * @version   1.0.3
+ * @version   1.0.4
  */
 
 namespace Craft;
@@ -24,34 +24,7 @@ class Htmlcache_HtmlcacheService extends BaseApplicationComponent
 
         $file = $this->getCacheFileName();
         if (file_exists($file)) {
-            // If file is older than 1 hour, delete it
-            // @todo 1.1: setting based
-            if (time() - ($fmt = filemtime($file)) >= 3600) {
-                unlink($file);
-                return;
-            }
-            $content = file_get_contents($file);
-
-            // Do something with the content
-            echo $content;
-
-            // Check the content type
-            $isJson = false;
-            if ($content[0] == '[' || $content[0] == '{') {
-                // JSON?
-                @json_decode($content);
-                if (json_last_error() == JSON_ERROR_NONE) {
-                    $isJson = true;
-                }
-            }
-
-            if ($isJson) {
-                // Add extra JSON headers?
-            }
-            else {
-                // Since it's most likely HTML, display small footprint
-                echo PHP_EOL . '<!-- Cached ' . date('Y-m-d H:i:s', $fmt) . ', printed ' . date('Y-m-d H:i:s') . ' -->';
-            }
+            \htmlcache_checkCache(false);
 
             return craft()->end();
         }
@@ -107,15 +80,11 @@ class Htmlcache_HtmlcacheService extends BaseApplicationComponent
     
     private function getCacheFileName($withDirectory = true)
     {
-        $fileName = preg_replace('/__(.+)?/i', '_', preg_replace('/[^a-z0-9]/i', '_', strtolower(trim($_SERVER['REQUEST_URI'], '/')))) . '.cached.html';
-        if ($withDirectory) {
-            $fileName = $this->getCacheFileDirectory() . $fileName;
-        }
-        return $fileName;
+        return \htmlcache_filename($withDirectory);
     }
     
     private function getCacheFileDirectory()
     {
-        return dirname(__DIR__) . DIRECTORY_SEPARATOR . '_cached' . DIRECTORY_SEPARATOR;
+        return \htmlcache_directory();
     }
 }
